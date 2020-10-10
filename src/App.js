308658075d1a5b8fd21dca1aa3e53774c106e1ab
Mohammed Asker <zetoanime@gmail.com>
2020-10-10 12:04:09 +0300

    added 4 weather boxes and make it responsive & deleted 1 image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from "react";
 import rain from "./icons/rain.png";
+import sunny from "./icons/sunny.png";
+
 import "./App.css";
 
 function App() {
 	return (
 		<div className="container">
-			<p>WED</p>
-			<img src={rain} />
-			<p>44°</p>
+			<div className="weather-box">
+				<p>WED</p>
+				<img src={sunny} alt="sunny" />
+				<p>35°</p>
+			</div>
+			<div className="weather-box">
+				<p>THU</p>
+				<img src={sunny} alt="sunny" />
+				<p>37°</p>
+			</div>
+			<div className="weather-box">
+				<p>FRI</p>
+				<img src={rain} alt="rainy" />
+				<p>41°</p>
+			</div>
+			<div className="weather-box">
+				<p>SAT</p>
+				<img src={sunny} alt="sunny" />
+				<p>44°</p>
+			</div>
+			<div className="weather-box">
+				<p>SUN</p>
+				<img src={rain} alt="rainy" />
+				<p>39°</p>
+			</div>
 		</div>
 	);
 }
