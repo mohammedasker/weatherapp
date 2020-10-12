0835d849a036ca45c1370fa107373cbdf7d1ce01
Mohammed Asker <zetoanime@gmail.com>
2020-10-12 12:12:53 +0300

    removed everything and replace it with simpler contents

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 import React from "react";
-import rain from "./icons/rain.png";
-import sunny from "./icons/sunny.png";
 
 import "./App.css";
 
 function App() {
 	return (
 		<div className="container">
-			<div className="weather-box">
-				<p>WED</p>
-				<img src={sunny} alt="sunny" />
-				<p>35°</p>
-			</div>
-			<div className="weather-box">
-				<p>THU</p>
-				<img src={sunny} alt="sunny" />
-				<p>37°</p>
-			</div>
-			<div className="weather-box">
-				<p>FRI</p>
-				<img src={rain} alt="rainy" />
-				<p>41°</p>
-			</div>
-			<div className="weather-box">
-				<p>SAT</p>
-				<img src={sunny} alt="sunny" />
-				<p>44°</p>
-			</div>
-			<div className="weather-box">
-				<p>SUN</p>
-				<img src={rain} alt="rainy" />
-				<p>39°</p>
-			</div>
+			<h1>Simple Weather App</h1>
+			<h3>Enter a city name</h3>
+			<input className="searchbar" />
+			<button className="submit-button">Submit</button>
 		</div>
 	);
 }
